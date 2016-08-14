3a229ae84925c45f3fa9cd2c1838fe96825c547c
Johannes Danneker <fry-@users.noreply.github.com>
2016-08-14 20:17:21 +0200

    make jump dependant on standing on ground

--- objects/player/scripts/rick.gd
+++ objects/player/scripts/rick.gd
@@ -2,12 +2,12 @@ extends RigidBody2D
 
 onready var init_pos = get_pos().x
 const FORCE = 200
-const JUMP_FORCE = -200
+const JUMP_FORCE = -280
 
 func _ready():
 	set_fixed_process(true)
 	
-func _fixed_process(delta):
+func _integrate_forces(state):
 	get_node("sprites").set_rotd((get_pos().x * -1)-init_pos)
 	
 	if Input.is_action_pressed("move_right"):
@@ -15,6 +15,5 @@ func _fixed_process(delta):
 	elif Input.is_action_pressed("move_left"):
 		set_linear_velocity(Vector2(-FORCE,get_linear_velocity().y))
 		
-	if Input.is_action_pressed("jump"):
-		if get_linear_velocity().y == 0:
-			set_linear_velocity(Vector2(get_linear_velocity().x,JUMP_FORCE))
+	if Input.is_action_pressed("jump") and (state.get_contact_count()>0):
+		set_linear_velocity(Vector2(get_linear_velocity().x,JUMP_FORCE))
